f2e24c9634b8ccb6780229a703246fc36bd025ea
Marvin E <Marvin.Etuk@gmail.com>
2022-11-17 19:08:05 +0000

    Add project files.

---
+++ App1/App1/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XCalendar.Core.Models;
+
+namespace App1.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public Calendar<CalendarDay> Calendar { get; set; } = new Calendar<CalendarDay>();
+    }
+}
---
+++ App1/App1/Views/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using App1.ViewModels;
+using Xamarin.Forms;
+
+namespace App1.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+        }
+    }
+}
